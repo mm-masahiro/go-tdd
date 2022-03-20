2679d3046d3486999aa24575089017ed6afab33c
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-03-20 14:23:51 +0900

    add second parameter

--- hello_test.go
+++ hello_test.go
@@ -9,16 +9,21 @@ func TestHello(t *testing.T) {
 			t.Errorf("got %q want %q", got, want)
 		}
 	}
-	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("Chris")
-		want := "Hello, Chris"
+	// t.Run("saying hello to people", func(t *testing.T) {
+	// 	got := Hello("Chris")
+	// 	want := "Hello, Chris"
+	// 	assertCorrectMessage(t, got, want)
+	// })
+
+	// t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+	// 	got := Hello("")
+	// 	want := "Hello, World"
+	// 	assertCorrectMessage(t, got, want)
+	// })
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
-
-	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, World"
-		assertCorrectMessage(t, got, want)
-	})
-
 }
--- main.go
+++ main.go
@@ -4,13 +4,17 @@ import "fmt"
 
 const prefix = "Hello, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == "Spanish" {
+		return "Hola, " + name
 	}
 	return prefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "English"))
 }
