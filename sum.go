5ed8d0bdd8a0c3293888cf3af2daed8b643164eb
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-03-20 20:47:08 +0900

    fix test

--- sum.go
+++ sum.go
@@ -8,3 +8,13 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	lengthOfNumbers := len(numbersToSum)
+	sums = make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return
+}
