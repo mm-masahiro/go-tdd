b4a2b9f4ed85c752f4554d789857e7605abbdb74
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-03-27 16:27:18 +0900

    refactor test

--- shapes/shapes_test.go
+++ shapes/shapes_test.go
@@ -3,30 +3,46 @@ package shapes
 import "testing"
 
 func TestArea(t *testing.T) {
-	checkArea := func(t *testing.T, shape AreaShape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
+
+	areaTests := []struct {
+		shape AreaShape
+		want  float64
+	}{
+		{Rectangle{12, 6}, 72.0},
+		{Circle{10}, 314.1592653589793},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
 
-	checkDiameter := func(t *testing.T, shape DiameterShape, want float64) {
-		t.Helper()
-		got := shape.Diameter()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
-	}
+	// checkArea := func(t *testing.T, shape AreaShape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
+	// checkDiameter := func(t *testing.T, shape DiameterShape, want float64) {
+	// 	t.Helper()
+	// 	got := shape.Diameter()
+	// 	if got != want {
+	// 		t.Errorf("got %g want %g", got, want)
+	// 	}
+	// }
 
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-		checkDiameter(t, circle, 20)
-	})
+	// t.Run("rectangles", func(t *testing.T) {
+	// 	rectangle := Rectangle{12.0, 6.0}
+	// 	checkArea(t, rectangle, 72.0)
+	// })
+
+	// t.Run("circles", func(t *testing.T) {
+	// 	circle := Circle{10}
+	// 	checkArea(t, circle, 314.1592653589793)
+	// 	checkDiameter(t, circle, 20)
+	// })
 }
