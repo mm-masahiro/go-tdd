4251721d6e5316cf18d0c3ebbbd74b8113eee398
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-04-02 11:25:49 +0900

    passed test

--- pointerTest/wallet.go
+++ pointerTest/wallet.go
@@ -1,6 +1,9 @@
 package pointertest
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type Bitcoin int
 
@@ -23,6 +26,11 @@ func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
-func (w *Wallet) Withdraw(amount Bitcoin) {
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("oh no")
+	}
+
 	w.balance -= amount
+	return nil
 }
--- pointerTest/wallet_test.go
+++ pointerTest/wallet_test.go
@@ -27,4 +27,16 @@ func TestWallet(t *testing.T) {
 
 		assertBalance(t, wallet, Bitcoin(10))
 	})
+
+	t.Run("Withdraw insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+
+		if err == nil {
+			t.Errorf("wanted an error but did'nt get one")
+		}
+	})
 }
