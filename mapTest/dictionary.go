a547a6f34dcb553968def039db2f7ba68cd15c07
mm-masahiro <hand5.mm9.mzh@gmail.com>
2022-04-08 22:55:19 +0900

    refactor test

--- mapTest/dictionary.go
+++ mapTest/dictionary.go
@@ -1,12 +1,16 @@
 package maptest
 
-import "errors"
-
 type Dictionary map[string]string
 
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+
 var (
-	ErrNotFound   = errors.New("could not find the word you were looking for")
-	ErrWordExists = errors.New("cannot add word because it already exists")
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("cannot add word because it already exists")
 )
 
 func (d Dictionary) Search(word string) (string, error) {
