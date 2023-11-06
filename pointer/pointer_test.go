0ff3a2e6cf69c279ff2d5b52cabae111d9d34284
mm-masahiro <hand5.mm9.mzh@gmail.com>
2023-11-06 21:01:29 +0900

    test refactor

--- pointer/pointer_test.go
+++ pointer/pointer_test.go
@@ -14,6 +14,13 @@ func TestWallet(t *testing.T) {
 			t.Errorf("got %s want %s", got, want)
 		}
 	}
+
+	assertError := func(t *testing.T, err error) {
+		t.Helper()
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
+	}
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
@@ -35,9 +42,6 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-
-		if err == nil {
-			t.Error("wanted an error but didn't get one")
-		}
+		assertError(t, err)
 	})
 }
